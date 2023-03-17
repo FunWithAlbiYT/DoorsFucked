ce8640e1ff890be0b60f98a68a1c1846b1be00d3
FunWithAlbi <68329886+FunWithAlbiYT@users.noreply.github.com>
2023-03-17 18:42:35 +0100

    Create hub.lua

---
+++ v1/hub.lua
@@ -0,0 +1,200 @@
+-- yeah
+
+local LMFAODOORSFUCKED666 = Instance.new("ScreenGui")
+local zaza = Instance.new("Frame")
+local PANANALL = Instance.new("Frame")
+local xxx = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local miniminalsex = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local labeltitlethingy = Instance.new("TextLabel")
+local UICorner_3 = Instance.new("UICorner")
+local speedydaddy = Instance.new("Frame")
+local valuess = Instance.new("TextLabel")
+local inc = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local december = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+
+LMFAODOORSFUCKED666.Name = "LMFAO DOORS FUCKED 666"
+LMFAODOORSFUCKED666.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+LMFAODOORSFUCKED666.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+zaza.Name = "zaza"
+zaza.Parent = LMFAODOORSFUCKED666
+zaza.BackgroundColor3 = Color3.fromRGB(54, 54, 54)
+zaza.BorderColor3 = Color3.fromRGB(0, 0, 0)
+zaza.BorderSizePixel = 3
+zaza.Position = UDim2.new(0.324369758, 0, 0.180781752, 0)
+zaza.Size = UDim2.new(0.315126061, 0, 0.687296391, 0)
+
+PANANALL.Name = "PANANALL"
+PANANALL.Parent = zaza
+PANANALL.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+PANANALL.BackgroundTransparency = 1.000
+PANANALL.BorderSizePixel = 0
+PANANALL.Size = UDim2.new(1, 0, 0.123222761, 0)
+
+xxx.Name = "xxx"
+xxx.Parent = PANANALL
+xxx.BackgroundColor3 = Color3.fromRGB(255, 0, 4)
+xxx.BorderSizePixel = 0
+xxx.Position = UDim2.new(0.920000076, 0, 0.15384616, 0)
+xxx.Size = UDim2.new(0, 22, 0, 22)
+xxx.Font = Enum.Font.SourceSans
+xxx.Text = ""
+xxx.TextColor3 = Color3.fromRGB(0, 0, 0)
+xxx.TextSize = 14.000
+
+UICorner.CornerRadius = UDim.new(1, 0)
+UICorner.Parent = xxx
+
+miniminalsex.Name = "miniminal sex"
+miniminalsex.Parent = PANANALL
+miniminalsex.BackgroundColor3 = Color3.fromRGB(255, 228, 21)
+miniminalsex.BorderSizePixel = 0
+miniminalsex.Position = UDim2.new(0.824000001, 0, 0.15384616, 0)
+miniminalsex.Size = UDim2.new(0, 22, 0, 22)
+miniminalsex.Font = Enum.Font.SourceSans
+miniminalsex.Text = ""
+miniminalsex.TextColor3 = Color3.fromRGB(0, 0, 0)
+miniminalsex.TextSize = 14.000
+
+UICorner_2.CornerRadius = UDim.new(1, 0)
+UICorner_2.Parent = miniminalsex
+
+labeltitlethingy.Name = "label title thingy"
+labeltitlethingy.Parent = PANANALL
+labeltitlethingy.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+labeltitlethingy.BackgroundTransparency = 1.000
+labeltitlethingy.Position = UDim2.new(0.018666666, 0, 0.15384616, 0)
+labeltitlethingy.Size = UDim2.new(0, 93, 0, 18)
+labeltitlethingy.Font = Enum.Font.Cartoon
+labeltitlethingy.Text = "DoorsFucked"
+labeltitlethingy.TextColor3 = Color3.fromRGB(255, 255, 255)
+labeltitlethingy.TextSize = 16.000
+labeltitlethingy.TextXAlignment = Enum.TextXAlignment.Left
+
+UICorner_3.Parent = zaza
+
+speedydaddy.Name = "speedy daddy"
+speedydaddy.Parent = zaza
+speedydaddy.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+speedydaddy.BackgroundTransparency = 1.000
+speedydaddy.Position = UDim2.new(0.0426666662, 0, 0.135071099, 0)
+speedydaddy.Size = UDim2.new(0, 128, 0, 38)
+
+valuess.Name = "valuess"
+valuess.Parent = speedydaddy
+valuess.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+valuess.BackgroundTransparency = 1.000
+valuess.Position = UDim2.new(0.00879774615, 0, 0.13333334, 0)
+valuess.Size = UDim2.new(0, 111, 0, 30)
+valuess.Font = Enum.Font.Cartoon
+valuess.Text = "Speed: 16"
+valuess.TextColor3 = Color3.fromRGB(255, 255, 255)
+valuess.TextSize = 14.000
+valuess.TextXAlignment = Enum.TextXAlignment.Left
+
+inc.Name = "inc"
+inc.Parent = speedydaddy
+inc.BackgroundColor3 = Color3.fromRGB(39, 39, 39)
+inc.BorderSizePixel = 0
+inc.Position = UDim2.new(0.54171133, 0, 0.177777797, 0)
+inc.Size = UDim2.new(0, 25, 0, 25)
+inc.Font = Enum.Font.SourceSans
+inc.Text = "+"
+inc.TextColor3 = Color3.fromRGB(255, 255, 255)
+inc.TextScaled = true
+inc.TextSize = 14.000
+inc.TextWrapped = true
+
+UICorner_4.CornerRadius = UDim.new(1, 0)
+UICorner_4.Parent = inc
+
+december.Name = "december"
+december.Parent = speedydaddy
+december.BackgroundColor3 = Color3.fromRGB(39, 39, 39)
+december.BorderSizePixel = 0
+december.Position = UDim2.new(0.786248922, 0, 0.177777797, 0)
+december.Size = UDim2.new(0, 25, 0, 25)
+december.Font = Enum.Font.SourceSans
+december.Text = "-"
+december.TextColor3 = Color3.fromRGB(255, 255, 255)
+december.TextScaled = true
+december.TextSize = 14.000
+december.TextWrapped = true
+
+UICorner_5.CornerRadius = UDim.new(1, 0)
+UICorner_5.Parent = december
+
+local function dascript()
+	local script = Instance.new('LocalScript', LMFAODOORSFUCKED666)
+
+	-- sexy script ikr
+	-- by albi ofcc
+	
+	local plr = game.Players.LocalPlayer
+	local uis = game:GetService('UserInputService')
+	local mouse = plr:GetMouse()
+	local speed = 16
+	local dragging = false
+	local open = true
+	
+	uis.InputBegan:Connect(function(inp)
+		if inp.KeyCode == Enum.KeyCode.RightShift then
+			if open == true then
+				open = false
+				script.Parent.zaza.Visible = false
+			else
+				open = true
+				script.Parent.zaza.Visible = true
+			end
+		end
+	end)
+	
+	mouse.Button1Down:Connect(function()
+		local things = plr.PlayerGui:GetGuiObjectsAtPosition(mouse.X, mouse.Y)
+		for key, thing in pairs(things) do
+			if thing == script.Parent.zaza.PANANALL then
+				dragging = true
+			end
+		end
+	end)
+	
+	mouse.Button1Up:Connect(function()
+		dragging = false
+	end)
+	
+	script.Parent.zaza.PANANALL.xxx.MouseButton1Click:Connect(function()
+		script.Parent:Destroy()
+	end)
+	
+	script.Parent.zaza.PANANALL["miniminal sex"].MouseButton1Click:Connect(function()
+		open = false
+		script.Parent.zaza.Visible = false
+	end)
+	
+	script.Parent.zaza["speedy daddy"].inc.MouseButton1Click:Connect(function()
+		if speed < 25 then
+			speed = speed+1
+			plr.Character.Humanoid.WalkSpeed = speed
+			script.Parent.zaza["speedy daddy"].valuess.Text = "Speed: "..tostring(speed)
+		end
+	end)
+	
+	script.Parent.zaza["speedy daddy"].december.MouseButton1Click:Connect(function()
+		if speed > 0 then
+			speed = speed-1
+			plr.Character.Humanoid.WalkSpeed = speed
+			script.Parent.zaza["speedy daddy"].valuess.Text = "Speed: "..tostring(speed)
+		end
+	end)
+	
+	while wait() do
+		if dragging == true then
+			script.Parent.zaza.Position = UDim2.new(0, mouse.X-200, 0, mouse.Y-10)
+		end
+	end
+end
+coroutine.wrap(dascript)()
